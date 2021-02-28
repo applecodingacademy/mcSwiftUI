3a916be42da6d509b8bbfcd6a3115f4c1a15ff94
Julio César Fernández <jcfmunoz@icloud.com>
2021-02-28 19:24:35 +0100

    Initial Commit

---
+++ mcSwiftUI/mcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcSwiftUIApp.swift
+//  mcSwiftUI
+//
+//  Created by Julio César Fernández Muñoz on 28/2/21.
+//
+
+import SwiftUI
+
+@main
+struct mcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
